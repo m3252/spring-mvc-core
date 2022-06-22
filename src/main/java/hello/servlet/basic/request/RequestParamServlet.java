80d7a1327e4077bcbf11224e1a5fcc9b5f36b007
noah <m3252@naver.com>
2022-06-23 00:18:56 +0900

    feat : GET 쿼리 파라미터

---
+++ src/main/java/hello/servlet/basic/request/RequestParamServlet.java
@@ -0,0 +1,42 @@
+package hello.servlet.basic.request;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Arrays;
+
+/**
+ * 1. 파라미터 전송 기능
+ * http://localhost:8080/request-param?username=hello&age=20
+ */
+
+@WebServlet(name = "requestParamServlet", urlPatterns = "/request-param")
+public class RequestParamServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("[전체 파라미터 조회] - start");
+
+        req.getParameterNames().asIterator()
+                        .forEachRemaining(paramName -> System.out.println(paramName + " = " + req.getParameter(paramName)));
+
+        System.out.println("[전체 파라미터 조회] - end");
+        System.out.println();
+
+        System.out.println("[단일 파라미터 조회] - start");
+
+        String username = req.getParameter("username");
+        System.out.println("username = " + username);
+
+        String age = req.getParameter("age");
+        System.out.println("age = " + age);
+
+        String[] usernames = req.getParameterValues("username");
+        Arrays.stream(usernames).forEach(System.out::println);
+
+        System.out.println("[단일 파라미터 조회] - end");
+    }
+}
