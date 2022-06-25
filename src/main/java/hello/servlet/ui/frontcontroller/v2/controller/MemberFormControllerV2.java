b132fa037f143cea1f2fd1499f1cc5052055d200
noah <m3252@naver.com>
2022-06-25 17:19:32 +0900

    feat : 프론트 컨트롤러 v2 도입

---
+++ src/main/java/hello/servlet/ui/frontcontroller/v2/controller/MemberFormControllerV2.java
@@ -0,0 +1,19 @@
+package hello.servlet.ui.frontcontroller.v2.controller;
+
+import hello.servlet.ui.frontcontroller.MyView;
+import hello.servlet.ui.frontcontroller.v1.ControllerV1;
+import hello.servlet.ui.frontcontroller.v2.ControllerV2;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class MemberFormControllerV2 implements ControllerV2 {
+
+    @Override
+    public MyView process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        return new MyView("/WEB-INF/views/new-form.jsp");
+    }
+}
